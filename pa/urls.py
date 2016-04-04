f671fa8485a6ff28863b9504a10accc5e113edbb
danleyb2 <ndieksman@gmail.com>
2016-04-04 15:30:28 +0000

    my index page pattern

--- pa/urls.py
+++ pa/urls.py
@@ -15,7 +15,7 @@ router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
+urlpatterns += [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
