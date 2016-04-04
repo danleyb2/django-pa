23a477257518fd96e66d9727a96ce57480af207f
danleyb2 <ndieksman@gmail.com>
2016-04-04 12:01:52 +0000

    init user profile creation

--- pa/models.py
+++ pa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User ,related_name='user_profile')
+    url = models.URLField("Website", blank=True)
+    company = models.CharField(max_length=50, blank=True)
+    
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
