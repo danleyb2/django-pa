aca314e599a41b61607909ba37aeeed66d9076c9
danleyb2 <ndieksman@gmail.com>
2016-04-05 02:44:24 +0000

    register UserProfile

--- pa/admin.py
+++ pa/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 # Register your models here.
+
+admin.site.unregister(User)
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [UserProfileInline, ]
+    
+admin.site.register(User,UserProfileAdmin)
+
