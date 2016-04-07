ed6a839769ea7c467fbcb1f271705c219a2ba485
danleyb2 <ndieksman@gmail.com>
2016-04-07 17:38:25 +0000

    add profile to user serializer

--- pa/serializers.py
+++ pa/serializers.py
@@ -1,14 +1,26 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import UserProfile
 
 
+class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
+    #full_name = Field(source='company')
+    class Meta:
+        model = UserProfile
+        fields = ('__all__')
+        
+        
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    profile = UserProfileSerializer(source="user_profile", read_only=True)
+    
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('username','url','email','profile')
+
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+        
